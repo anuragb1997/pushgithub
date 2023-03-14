2668765431064408e8b6d35629fbd44a31767156
anuragb1997 <101621536+anuragb1997@users.noreply.github.com>
2023-03-14 17:27:54 +0530

    Add files via upload

---
+++ lab9/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (e) => {
+    console.log("installed");
+})
+self.addEventListener("activate", (e) => {
+    console.log("activated");
+})
+self.addEventListener("fetch", (e) => {
+    console.log("fetched");
+    // fetching user data from the github api
+    fetch("https://api.github.com/users/kunal-kushwaha")
+    .then((e) => {
+    return e.json();
+    })
+    .then((json) => {
+    console.log(json);
+    })
+    .catch((e) => {
+    console.log(e);
+    });
+})
